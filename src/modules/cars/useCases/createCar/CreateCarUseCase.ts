fefac37be2f5cd4d4111f26934eaf9db149cb99c
Yago Gomes <yagogomesc@gmail.com>
2022-08-31 23:22:47 -0300

    Cria controller e use case de criar carro

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -0,0 +1,40 @@
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { inject, injectable } from 'tsyringe';
+
+import { AppError } from '@shared/errors/AppError';
+
+interface IRequest {
+    name: string;
+    description: string;
+    daily_rate: number;
+    license_plate: string;
+    fine_amount: number;
+    brand: string;
+    category_id: string;
+}
+
+@injectable()
+class CreateCarUseCase {
+  constructor(
+        @inject('CarsRepository')
+        private carsRepository: ICarsRepository,
+  ) {}
+
+  async execute({
+    name, description, brand, category_id, daily_rate, fine_amount, license_plate,
+  }: IRequest): Promise<Car> {
+    const carAlreadyExists = await this.carsRepository.findByLicensePlate(license_plate);
+
+    if (carAlreadyExists) {
+      throw new AppError('Car already exists');
+    }
+    const car = await this.carsRepository.create({
+      name, description, brand, category_id, daily_rate, fine_amount, license_plate,
+    });
+
+    return car;
+  }
+}
+
+export { CreateCarUseCase };
