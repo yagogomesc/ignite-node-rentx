7c2d2201df5e0de119f8cc7ccbed6ac538c1957e
Yago Gomes <yagogomesc@gmail.com>
2022-09-02 21:15:05 -0300

    Refatora conexão ao db e cria seed de admin

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,31 +1,10 @@
-import { createConnection, getConnectionOptions } from 'typeorm';
+import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-import { Category } from '../modules/cars/entities/Category';
-import { Specification } from '../modules/cars/entities/Specification';
-import { CreateCategories1660347706753 } from './migrations/1660347706753-CreateCategories';
-
-interface IOptions {
-  host: string;
-}
-
-getConnectionOptions().then((options) => {
-  const newOptions = options as IOptions;
-  newOptions.host = 'database_ignite';
-  createConnection({ ...options });
-});
-
-// const dataSource = new DataSource({
-//   type: 'postgres',
-//   port: 5432,
-//   username: 'docker',
-//   password: 'ignite',
-//   database: process.env.NODE_ENV === 'test' ? 'rentx_test' : 'rentx',
-//   entities: [Category, Specification],
-//   migrations: [CreateCategories1660347706753],
-// });
-
-// export function createConnection(host = 'database'): Promise<DataSource> {
-//   return dataSource.setOptions({ host }).initialize();
-// }
-
-// export default dataSource;
+export default async (host = 'database'): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+  return createConnection(
+    Object.assign(defaultOptions, {
+      host,
+    }),
+  );
+};
