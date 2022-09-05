3b16b0d65038077e25f47fc85141dd8eb44721e7
Yago Gomes <yagogomesc@gmail.com>
2022-09-05 20:06:34 -0300

    Inclui middleware valida que é admin

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -4,6 +4,9 @@ import { ListCategoriesController } from '@modules/cars/useCases/listCategories/
 import { Router } from 'express';
 import multer from 'multer';
 
+import { ensureAdmin } from '../middlewares/ensureAdmin';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+
 const categoriesRoutes = Router();
 
 const upload = multer({
@@ -14,10 +17,10 @@ const createCategoryController = new CreateCategoryController();
 const importCategoryController = new ImportCategoryController();
 const listCategoriesController = new ListCategoriesController();
 
-categoriesRoutes.post('/', createCategoryController.handle);
+categoriesRoutes.post('/', ensureAuthenticated, ensureAdmin, createCategoryController.handle);
 
 categoriesRoutes.get('/', listCategoriesController.handle);
 
-categoriesRoutes.post('/import', upload.single('file'), importCategoryController.handle);
+categoriesRoutes.post('/import', ensureAuthenticated, ensureAdmin, upload.single('file'), importCategoryController.handle);
 
 export { categoriesRoutes };
