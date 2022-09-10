178cff0b4af2a90270fe3a21b49c2425779f15f8
Yago Gomes <yagogomesc@gmail.com>
2022-09-09 21:46:33 -0300

    Ajusta nomes e finaliza upload de imagens

--- src/modules/cars/infra/typeorm/repositories/CarsImagesRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsImagesRepository.ts
@@ -3,7 +3,7 @@ import { getRepository, Repository } from 'typeorm';
 
 import { CarImage } from '../entities/CarImage';
 
-class CarsImageRepository implements ICarsImagesRepository {
+class CarsImagesRepository implements ICarsImagesRepository {
   private repository: Repository<CarImage>;
 
   constructor() {
@@ -22,4 +22,4 @@ class CarsImageRepository implements ICarsImagesRepository {
   }
 }
 
-export { CarsImageRepository };
+export { CarsImagesRepository };
--- src/modules/cars/useCases/uploadCarImage/UploadCarImageController.ts
+++
@@ -1,5 +0,0 @@
-class UploadCarImageController {
-
-}
-
-export { UploadCarImageController };
