76ab7cb6a9097fea59f397198fafea5074c0bea1
Yago Gomes <yagogomesc@gmail.com>
2022-09-09 21:07:28 -0300

    Criação de especificação para o carro

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -7,12 +7,12 @@ import { ICarsRepository } from '../ICarsRepository';
 class CarsRepositoryInMemory implements ICarsRepository {
   cars: Car[] = [];
   async create({
-    brand, category_id, daily_rate, description, fine_amount, license_plate, name,
+    brand, category_id, daily_rate, description, fine_amount, license_plate, name, id,
   }: ICreateCarDTO): Promise<Car> {
     const car = new Car();
 
     Object.assign(car, {
-      brand, category_id, daily_rate, description, fine_amount, license_plate, name,
+      brand, category_id, daily_rate, description, fine_amount, license_plate, name, id,
     });
 
     this.cars.push(car);
@@ -34,6 +34,12 @@ class CarsRepositoryInMemory implements ICarsRepository {
     });
 
     return cars;
+  }
+
+  async findById(id: string): Promise<Car> {
+    const car = this.cars.find((car) => car.id === id);
+
+    return car;
   }
 }
 
