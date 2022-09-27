2447422075b96722c5848b915481ef574957d4a0
Yago Gomes <yagogomesc@gmail.com>
2022-09-26 21:46:03 -0300

    Cria relacionamento de aluguel com carro

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -1,5 +1,6 @@
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
 import {
-  Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn,
+  Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn, UpdateDateColumn,
 } from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 
@@ -7,6 +8,10 @@ import { v4 as uuidV4 } from 'uuid';
 class Rental {
   @PrimaryColumn()
     id: string;
+
+  @ManyToOne(() => Car)
+  @JoinColumn({ name: 'car_id' })
+    car: Car;
 
   @Column()
     car_id: string;
