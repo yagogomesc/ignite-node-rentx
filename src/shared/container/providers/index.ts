06ef3e3f616d54ad4968a3c203c3e020f95c881d
Yago Gomes <yagogomesc@gmail.com>
2022-10-14 21:09:54 -0300

    Implementando upload no s3

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -5,8 +5,14 @@ import { DayjsDateProvider } from './DateProvider/implementations/DayjsDateProvi
 import { IMailProvider } from './MailProvider/IMailProvider';
 import { EtherealMailProvider } from './MailProvider/implementations/EtherealMailProvider';
 import { LocalStorageProvider } from './StorageProvider/implementations/LocalStorageProvider';
+import { S3StorageProvider } from './StorageProvider/implementations/S3StorageProvider';
 import { IStorageProvider } from './StorageProvider/IStorageProvider';
 
 container.registerSingleton<IDateProvider>('DayjsDateProvider', DayjsDateProvider);
 container.registerInstance<IMailProvider>('EtherealMailProvider', new EtherealMailProvider());
-container.registerSingleton<IStorageProvider>('StorageProvider', LocalStorageProvider);
+
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: S3StorageProvider,
+};
+container.registerSingleton<IStorageProvider>('StorageProvider', diskStorage[process.env.disk]);
