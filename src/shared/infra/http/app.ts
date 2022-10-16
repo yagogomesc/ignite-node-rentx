91b7eaf4893bc8004687397ddb49dfce8aca8d6f
Yago Gomes <yagogomesc@gmail.com>
2022-10-16 16:16:06 -0300

    Cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import 'dotenv/config';
 
 import upload from '@config/upload';
+import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
 import swaggerUi from 'swagger-ui-express';
 import 'express-async-errors';
@@ -22,6 +23,8 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
+
+app.use(cors());
 
 app.use(router);
 
